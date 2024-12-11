b123e5d52159b8cd0a5004fd4a89db53f80b814f
ibrahimagdy <iibbrraahhimm00@gmail.com>
2024-12-11 21:57:10 +0200

    Add dependencies Add splash screen

---
+++ lib/solidify.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:solidify/core/routes/app_routes.dart';
+import 'core/theming/color_manger.dart';
+
+class SolidifyApp extends StatelessWidget {
+  final AppRoutes appRoutes;
+  final String initialRoute;
+
+  const SolidifyApp({
+    super.key,
+    required this.appRoutes,
+    required this.initialRoute,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(393, 852),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'SOLIDIFY',
+        theme: ThemeData(
+          primaryColor: ColorsManager.mainBlue,
+          scaffoldBackgroundColor: ColorsManager.scaffoldBackground,
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: initialRoute,
+        onGenerateRoute: appRoutes.generateRoute,
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,15 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:solidify/core/routes/app_routes.dart';
 
 import 'package:solidify/main.dart';
+import 'package:solidify/solidify.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(SolidifyApp(appRoutes: AppRoutes(), initialRoute: '/'));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
