c05d702913dc9dc2608b2069dade95c6c5c9e689
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2024-12-15 03:09:25 +0200

    change start route

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:solidify/solidify.dart';
 import 'package:solidify/core/routes/app_routes.dart';
 import 'package:solidify/core/routes/routes_name.dart';
-import 'package:solidify/solidify.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     SolidifyApp(
       appRoutes: AppRoutes(),
-      initialRoute: Routes.splashScreen,
+      initialRoute: Routes.loginScreen,
     ),
   );
 }
-
