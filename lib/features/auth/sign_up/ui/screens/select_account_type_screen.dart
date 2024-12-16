c12b099685c22d9f30df94fe781614401b501eb7
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2024-12-16 06:27:45 +0200

    start design

--- lib/features/auth/sign_up/ui/screens/select_account_type_screen.dart
+++ lib/features/auth/sign_up/ui/screens/select_account_type_screen.dart
@@ -1,4 +1,9 @@
+import 'package:flutter_svg/svg.dart';
 import 'package:flutter/material.dart';
+import 'package:solidify/core/helpers/spacing.dart';
+import 'package:solidify/core/helpers/extensions.dart';
+import 'package:solidify/core/theming/text_styles.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SelectAccountTypeScreen extends StatefulWidget {
   const SelectAccountTypeScreen({super.key});
@@ -11,6 +16,30 @@ class SelectAccountTypeScreen extends StatefulWidget {
 class _SelectAccountTypeScreenState extends State<SelectAccountTypeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () {
+                context.pop();
+              },
+              child: SvgPicture.asset(
+                "assets/svgs/back_button.svg",
+                fit: BoxFit.scaleDown,
+              ),
+            ),
+            verticalSpace(35),
+            Text(
+              'Create your account as',
+              style: TextStyles.font24MainBlueMedium,
+            ),
+          ],
+        ),
+      )),
+    );
   }
 }
