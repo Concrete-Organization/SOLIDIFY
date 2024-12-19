4933a878d877cf0c7b0d6adfd9da668a8c903c1b
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2024-12-19 03:24:20 +0200

    eng sign up screen initail  design

---
+++ lib/features/auth/sign_up/ui/screens/engineer_account_sign_up/identity_auth.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IdentityAuth extends StatefulWidget {
+  const IdentityAuth({super.key});
+
+  @override
+  State<IdentityAuth> createState() => _IdentityAuthState();
+}
+
+class _IdentityAuthState extends State<IdentityAuth> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
