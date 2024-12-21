b5d2dca11ad6e0635736b3ebf7af7c521b3f77a7
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2024-12-21 07:35:48 +0200

    create business account list

--- lib/features/auth/sign_up/screens/business_account_sign_up/ui/business_account_sign_up_screen.dart
+++ lib/features/auth/sign_up/screens/business_account_sign_up/ui/business_account_sign_up_screen.dart
@@ -6,6 +6,7 @@ import 'package:solidify/core/theming/color_manger.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:solidify/core/widgets/app_text_form_field.dart';
 import 'package:solidify/features/auth/sign_up/screens/engineer_sign_up/widgets/engineer_account_app_bar.dart';
+import 'package:solidify/features/auth/sign_up/screens/business_account_sign_up/ui/widgets/business_acount_sign_up_form.dart';
 
 class BusinessAccountSignUpScreen extends StatelessWidget {
   const BusinessAccountSignUpScreen({super.key});
@@ -16,21 +17,24 @@ class BusinessAccountSignUpScreen extends StatelessWidget {
       body: SafeArea(
           child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            verticalSpace(20),
-            const EngineerAccountAppBar(currentIndex: 0, totalPages: 3),
-            verticalSpace(30),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Identity authentication',
-                style: TextStyles.font24MainBlueSemiBold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              verticalSpace(20),
+              const EngineerAccountAppBar(currentIndex: 0, totalPages: 3),
+              verticalSpace(30),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Identity authentication',
+                  style: TextStyles.font24MainBlueSemiBold,
+                ),
               ),
-            ),
-            verticalSpace(40),
-          ],
+              verticalSpace(40),
+              const BusinessAcountSignUpForm()
+            ],
+          ),
         ),
       )),
     );
---
+++ lib/features/auth/sign_up/screens/business_account_sign_up/ui/financial_details_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FinancialDetailsScreen extends StatefulWidget {
+  const FinancialDetailsScreen({super.key});
+
+  @override
+  State<FinancialDetailsScreen> createState() => _FinancialDetailsScreenState();
+}
+
+class _FinancialDetailsScreenState extends State<FinancialDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
