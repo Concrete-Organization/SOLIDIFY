a1ec6395ac9a6256842746eb23db17e11e82e6cc
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2024-12-21 07:24:12 +0200

    extract global widget

---
+++ lib/features/auth/sign_up/screens/business_account_sign_up/ui/business_account_sign_up_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter/material.dart';
+import 'package:solidify/core/helpers/spacing.dart';
+import 'package:solidify/core/theming/text_styles.dart';
+import 'package:solidify/core/theming/color_manger.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:solidify/core/widgets/app_text_form_field.dart';
+import 'package:solidify/features/auth/sign_up/screens/engineer_sign_up/widgets/engineer_account_app_bar.dart';
+
+class BusinessAccountSignUpScreen extends StatelessWidget {
+  const BusinessAccountSignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            verticalSpace(20),
+            const EngineerAccountAppBar(currentIndex: 0, totalPages: 3),
+            verticalSpace(30),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Identity authentication',
+                style: TextStyles.font24MainBlueSemiBold,
+              ),
+            ),
+            verticalSpace(40),
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/features/auth/sign_up/screens/business_account_sign_up/ui/widgets/business_acount_sign_up_form.dart
@@ -0,0 +1,75 @@
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter/material.dart';
+import 'package:solidify/core/helpers/spacing.dart';
+import 'package:solidify/core/theming/text_styles.dart';
+import 'package:solidify/core/theming/color_manger.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:solidify/core/widgets/app_text_form_field.dart';
+
+class BusinessAcountSignUpForm extends StatelessWidget {
+  const BusinessAcountSignUpForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+      children: [
+        Text(
+          'Company official name ',
+          style: TextStyles.font14lightBlackRegular,
+        ),
+        verticalSpace(5),
+        const AppTextFormField(hintText: 'Enter company name'),
+        verticalSpace(10),
+        Text(
+          'Company address ',
+          style: TextStyles.font14lightBlackRegular,
+        ),
+        verticalSpace(5),
+        const AppTextFormField(hintText: 'Enter Company address '),
+        verticalSpace(10),
+        Text(
+          'Commercial registration number ',
+          style: TextStyles.font14lightBlackRegular,
+        ),
+        verticalSpace(5),
+        const AppTextFormField(
+            hintText: 'Enter Commercial registration number'),
+        verticalSpace(10),
+        Text(
+          'Tax ID ',
+          style: TextStyles.font14lightBlackRegular,
+        ),
+        verticalSpace(5),
+        const AppTextFormField(hintText: 'Only 12 numbers'),
+        verticalSpace(10),
+        Text(
+          'Copy of the commercial license',
+          style: TextStyles.font14lightBlackRegular,
+        ),
+        verticalSpace(5),
+        Container(
+          width: 353.w,
+          height: 62.h,
+          decoration: BoxDecoration(
+              border: Border.all(color: ColorsManager.mainBlue),
+              borderRadius: BorderRadius.circular(5.r),
+              color: ColorsManager.mainBlueWith15Opacity),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.w),
+            child: Row(
+              children: [
+                Text(
+                  'Upload here',
+                  style: TextStyles.font12lightBlackLight,
+                ),
+                const Spacer(),
+                SvgPicture.asset('assets/svgs/upload_button.svg')
+              ],
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+}
