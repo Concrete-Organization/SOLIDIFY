60bd6e5afeec690212bc39b3fec4429559bfc9db
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2024-12-21 09:47:57 +0200

    finsh third screen

--- lib/features/auth/sign_up/screens/business_account_sign_up/ui/financial_details_screen.dart
+++ lib/features/auth/sign_up/screens/business_account_sign_up/ui/financial_details_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:solidify/core/helpers/spacing.dart';
+import 'package:solidify/core/theming/text_styles.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:solidify/core/widgets/app_text_button.dart';
+import 'package:solidify/core/widgets/app_text_form_field.dart';
+import 'package:solidify/features/auth/sign_up/screens/engineer_sign_up/widgets/engineer_account_app_bar.dart';
 
 class FinancialDetailsScreen extends StatefulWidget {
   const FinancialDetailsScreen({super.key});
@@ -19,12 +24,53 @@ class _FinancialDetailsScreenState extends State<FinancialDetailsScreen> {
               child: SingleChildScrollView(
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: const Column(
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [],
+                children: [
+                  verticalSpace(20),
+                  const EngineerAccountAppBar(currentIndex: 2, totalPages: 3),
+                  verticalSpace(40),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Financial details ',
+                      style: TextStyles.font24MainBlueSemiBold,
+                    ),
+                  ),
+                  verticalSpace(45),
+                  Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Company’s bank account',
+                          style: TextStyles.font14lightBlackRegular,
+                        ),
+                        verticalSpace(10),
+                        const AppTextFormField(
+                            hintText: 'Enter Company’s bank account'),
+                        verticalSpace(20),
+                        Text(
+                          'Company’s terms for payment and delivery',
+                          style: TextStyles.font14lightBlackRegular,
+                        ),
+                        verticalSpace(10),
+                        const AppTextFormField(
+                            hintText: 'Enter company’s terms'),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
-          ))
+          )),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+            child: AppTextButton(
+              onPressed: () {},
+              textButton: 'Continue',
+            ),
+          ),
         ],
       )),
     );
