9e375468bbf632ba60a1680278321fe247bea520
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2024-12-24 02:46:53 +0200

    create api service

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -1,8 +1,10 @@
+import 'api_constants.dart';
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 import '../../features/auth/login/data/model/login_request_body.dart';
 import '../../features/auth/login/data/model/login_response_body.dart';
-import 'api_constants.dart';
+import 'package:solidify/features/auth/sign_up/screens/user_sign_up/data/models/user_sign_up_request_model.dart';
+import 'package:solidify/features/auth/sign_up/screens/user_sign_up/data/models/user_sign_up_response_model.dart';
 part 'api_service.g.dart';
 
 @RestApi(baseUrl: ApiConstants.apiBaseUrl)
@@ -12,5 +14,10 @@ abstract class ApiService {
   @POST(ApiConstants.login)
   Future<LoginResponseBody> login(
     @Body() LoginRequestBody loginRequestBody,
+  );
+
+  @POST(ApiConstants.userSignUP)
+  Future<UserSignUpResponseModel> userSignUp(
+    @Body() UserSignUpRequestModel loginRequestBody,
   );
 }
