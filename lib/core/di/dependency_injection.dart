a4a625064ec5947a1f2e4e0817ab110eb1068eda
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2024-12-24 02:53:33 +0200

    create the repo and inject it

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,10 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-
-import '../../features/auth/login/data/repo/login_repo.dart';
-import '../../features/auth/login/logic/login_cubit.dart';
 import '../network/api_service.dart';
 import '../network/dio_factory.dart';
+import '../../features/auth/login/logic/login_cubit.dart';
+import '../../features/auth/login/data/repo/login_repo.dart';
+import 'package:solidify/features/auth/sign_up/screens/user_sign_up/data/repos/user_sign_up_repo.dart';
 
 final getIt = GetIt.instance;
 Future<void> setupGetIt() async {
@@ -15,4 +15,7 @@ Future<void> setupGetIt() async {
   // login
   getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
   getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
+
+//user sign up
+  getIt.registerLazySingleton<UserSignUpRepo>(() => UserSignUpRepo(getIt()));
 }
