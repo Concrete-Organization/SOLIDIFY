6af4bd32b95a79985ceaf3a739b385d7bada807d
ibrahimagdy <iibbrraahhimm00@gmail.com>
2025-01-27 20:35:35 +0200

    Refactor business upload license

--- lib/features/auth/sign_up/screens/business_account_sign_up/ui/widgets/identity_auth_form.dart
+++ lib/features/auth/sign_up/screens/business_account_sign_up/ui/widgets/identity_auth_form.dart
@@ -1,27 +1,81 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:solidify/core/helpers/spacing.dart';
 import 'package:solidify/core/theming/text_styles.dart';
 import 'package:solidify/core/widgets/app_text_form_field.dart';
 import 'package:solidify/features/auth/sign_up/screens/engineer_sign_up/ui/widgets/upload_container.dart';
 import 'package:solidify/features/auth/sign_up/screens/business_account_sign_up/logic/business_sign_up_cubit.dart';
-// features/auth/sign_up/screens/business_account_sign_up/ui/widgets/identity_auth_form.dart
 
-class IdentityAuthForm extends StatelessWidget {
+import '../../../engineer_sign_up/ui/engineer_sign_up_upload_files_screen.dart';
+
+class IdentityAuthForm extends StatefulWidget {
   const IdentityAuthForm({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final cubit = context.read<BusinessAccountSignUpCubit>();
+  State<IdentityAuthForm> createState() => _IdentityAuthFormState();
+}
 
+class _IdentityAuthFormState extends State<IdentityAuthForm> {
+  String? commercialLicenseType;
+  late BusinessAccountSignUpCubit cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cubit = context.read<BusinessAccountSignUpCubit>();
+  }
+
+  Future<File> saveFileToTempDirectory(PlatformFile platformFile) async {
+    final tempDir = await getTemporaryDirectory();
+    final file = File('${tempDir.path}/${platformFile.name}');
+    return file.writeAsBytes(platformFile.bytes!);
+  }
+
+  void updateFileState(File file, String fileType) {
+    setState(() {
+        cubit.commercialLicenseFile = file;
+        commercialLicenseType = fileType;
+    });
+  }
+
+  Future<void> navigateToUploadScreen(bool isCV) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const EngineerSignUpUploadFilesScreen(),
+      ),
+    );
+
+    if (result != null && result is Map<String, String>) {
+      final fileName = result['name'];
+      final fileType = result['extension'];
+
+      if (fileName != null && fileType != null) {
+        final file = File(fileName);
+        if (await file.exists()) {
+          updateFileState(file, fileType,);
+        } else {
+          print("File does not exist: $fileName");
+        }
+      }
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
     return Form(
       key: cubit.identityFormKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Company Name
-          Text('Company official name',
-              style: TextStyles.font14lightBlackRegular),
+          Text(
+            'Company official name',
+            style: TextStyles.font14lightBlackRegular,
+          ),
           verticalSpace(5),
           AppTextFormField(
             controller: cubit.companyNameController,
@@ -32,8 +86,10 @@ class IdentityAuthForm extends StatelessWidget {
 
           verticalSpace(18),
 
-          // Address
-          Text('Company address', style: TextStyles.font14lightBlackRegular),
+          Text(
+            'Company address',
+            style: TextStyles.font14lightBlackRegular,
+          ),
           verticalSpace(5),
           AppTextFormField(
             controller: cubit.addressController,
@@ -43,9 +99,10 @@ class IdentityAuthForm extends StatelessWidget {
 
           verticalSpace(18),
 
-          // Commercial Number
-          Text('Commercial registration number',
-              style: TextStyles.font14lightBlackRegular),
+          Text(
+            'Commercial registration number',
+            style: TextStyles.font14lightBlackRegular,
+          ),
           verticalSpace(5),
           AppTextFormField(
             controller: cubit.commercialNumberController,
@@ -55,9 +112,10 @@ class IdentityAuthForm extends StatelessWidget {
           ),
 
           verticalSpace(18),
-
-          // Tax ID
-          Text('Tax ID', style: TextStyles.font14lightBlackRegular),
+          Text(
+            'Tax ID',
+            style: TextStyles.font14lightBlackRegular,
+          ),
           verticalSpace(5),
           AppTextFormField(
             controller: cubit.taxIdController,
@@ -70,12 +128,25 @@ class IdentityAuthForm extends StatelessWidget {
           ),
 
           verticalSpace(18),
-
-          // Commercial License
-          Text('Copy of the commercial license',
-              style: TextStyles.font14lightBlackRegular),
+          Text(
+            'Copy of the commercial license',
+            style: TextStyles.font14lightBlackRegular,
+          ),
           verticalSpace(5),
-          const UploadContainer(),
+          GestureDetector(
+            onTap: () => navigateToUploadScreen(true),
+            child: UploadContainer(
+              isUploaded: cubit.commercialLicenseFile != null,
+              uploadedFileName: cubit.commercialLicenseFile?.path.split('/').last,
+              fileType: commercialLicenseType,
+              onClose: () {
+                setState(() {
+                  cubit.commercialLicenseFile = null;
+                  commercialLicenseType = null;
+                });
+              },
+            ),
+          ),
         ],
       ),
     );
