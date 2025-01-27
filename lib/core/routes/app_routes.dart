ec69c1091f1ec3b5f0bee513a1873aeb8182d15f
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-01-27 17:21:00 +0200

    inject and put the provider on the route

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:solidify/features/auth/forget_password/logic/forget_password_cubit.dart';
-import 'package:solidify/features/auth/otp/logic/verify_otp_cubit.dart';
-import 'package:solidify/features/auth/reset_password/logic/reset_password_cubit.dart';
-import 'package:solidify/features/auth/sign_up/screens/engineer_sign_up/logic/engineer_sign_up_cubit.dart';
-import 'package:solidify/features/auth/sign_up/screens/engineer_sign_up/ui/engineer_sign_up_upload_files_screen.dart';
 import '../di/dependency_injection.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:solidify/core/routes/routes_name.dart';
@@ -13,15 +8,22 @@ import 'package:solidify/features/auth/otp/ui/otp_screen.dart';
 import 'package:solidify/features/auth/login/ui/login_screen.dart';
 import 'package:solidify/features/auth/login/logic/login_cubit.dart';
 import 'package:solidify/features/onboarding/onboarding_screen.dart';
+import 'package:solidify/features/auth/otp/logic/verify_otp_cubit.dart';
 import 'package:solidify/features/auth/reset_password/ui/reset_password_screen.dart';
+import 'package:solidify/features/auth/reset_password/logic/reset_password_cubit.dart';
 import 'package:solidify/features/auth/forget_password/ui/forget_password_screen.dart';
+import 'package:solidify/features/auth/forget_password/logic/forget_password_cubit.dart';
 import 'package:solidify/features/auth/sign_up/screens/user_sign_up/ui/user_sign_up_screen.dart';
 import 'package:solidify/features/auth/sign_up/screens/user_sign_up/logic/user_sign_up_cubit.dart';
 import 'package:solidify/features/auth/sign_up/screens/engineer_sign_up/ui/engineer_sign_up_screen.dart';
+import 'package:solidify/features/auth/sign_up/screens/engineer_sign_up/logic/engineer_sign_up_cubit.dart';
 import 'package:solidify/features/auth/sign_up/screens/select_account_type/ui/select_account_type_screen.dart';
 import 'package:solidify/features/auth/sign_up/screens/business_account_sign_up/ui/business_sign_up_screen.dart';
+import 'package:solidify/features/auth/sign_up/screens/business_account_sign_up/logic/business_sign_up_cubit.dart';
 import 'package:solidify/features/auth/sign_up/screens/business_account_sign_up/ui/widgets/contact_info_screen.dart';
+import 'package:solidify/features/auth/sign_up/screens/engineer_sign_up/ui/engineer_sign_up_upload_files_screen.dart';
 import 'package:solidify/features/auth/sign_up/screens/business_account_sign_up/ui/widgets/financial_details_screen.dart';
+import 'package:solidify/features/auth/sign_up/screens/business_account_sign_up/logic/business_account_sign_up_state.dart';
 
 class AppRoutes {
   Route generateRoute(RouteSettings settings) {
@@ -86,8 +88,10 @@ class AppRoutes {
         );
       case Routes.businessAccountScreen:
         return MaterialPageRoute(
-          builder: (context) => const BusinessSignUpScreen(),
-        );
+            builder: (context) => BlocProvider(
+                  create: (context) => getIt<BusinessAccountSignUpCubit>(),
+                  child: const BusinessSignUpScreen(),
+                ));
       case Routes.financialDetailsScreen:
         return MaterialPageRoute(
           builder: (context) => const FinancialDetailsScreen(),
