6a035784a5b25e6129e1e9d352605b7dac85a4f3
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-03-06 07:42:44 +0200

    create the api call

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -2,6 +2,8 @@ class ApiConstants {
   static const String apiBaseUrl = "https://solidifyapi.runasp.net/api/";
   static const String concreteStrengthAiApiBaseUrl =
       "https://mahmoud763-concrete.hf.space/";
+  static const String geminiApiUrl =
+      "https://generativelanguage.googleapis.com/v1beta/";
 
   static const String login = "Account/login";
   static const String engineerSignUP = 'Account/registerEngineer';
@@ -13,24 +15,6 @@ class ApiConstants {
   static const String predict = 'predict';
   static const String product = 'Product';
 
-  static const String geminiApiUrl =
-      "https://generativelanguage.googleapis.com/v1beta/";
-}
-
-class ApiErrors {
-  static const String badRequestError = "badRequestError";
-  static const String noContent = "noContent";
-  static const String forbiddenError = "forbiddenError";
-  static const String unauthorizedError = "unauthorizedError";
-  static const String notFoundError = "notFoundError";
-  static const String conflictError = "conflictError";
-  static const String internalServerError = "internalServerError";
-  static const String unknownError = "unknownError";
-  static const String timeoutError = "timeoutError";
-  static const String defaultError = "defaultError";
-  static const String cacheError = "cacheError";
-  static const String noInternetError = "noInternetError";
-  static const String loadingMessage = "loading_message";
-  static const String retryAgainMessage = "retry_again_message";
-  static const String ok = "Ok";
+  /// Returns the endpoint for a category with the given [id].
+  static String categoryEndpoint(int id) => "Category/$id";
 }
--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -1,18 +1,23 @@
-import 'package:solidify/features/auth/sign_up/screens/concrete_company_account_sign_up/data/models/concrete_company_account_sign_up_response_model.dart';
-import 'package:solidify/features/auth/sign_up/screens/engineer_account_sign_up/data/models/engineer_account_sign_up_request_model.dart';
-import 'package:solidify/features/auth/sign_up/screens/engineer_account_sign_up/data/models/engineer_account_sign_up_response_model.dart';
-import 'package:solidify/features/marketplace/data/models/product_list_response_model.dart';
 import 'api_constants.dart';
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
-import '../../features/auth/login/data/model/login_request_body.dart';
-import '../../features/auth/login/data/model/login_response_body.dart';
+import 'package:solidify/features/auth/login/data/model/login_request_body.dart';
+import 'package:solidify/features/auth/login/data/model/login_response_body.dart';
 import 'package:solidify/features/auth/otp/data/models/verify_otp_request_model.dart';
 import 'package:solidify/features/auth/otp/data/models/verify_otp_response_model.dart';
+import 'package:solidify/features/marketplace/data/models/product_list_response_model.dart';
 import 'package:solidify/features/auth/reset_password/data/models/reset_password_request_model.dart';
 import 'package:solidify/features/auth/reset_password/data/models/reset_password_response_model.dart';
 import 'package:solidify/features/auth/forget_password/data/models/forget_password_request_model.dart';
 import 'package:solidify/features/auth/forget_password/data/models/forget_password_response_model.dart';
+import 'package:solidify/features/marketplace/data/models/get_products_by_category_response_model.dart';
+import 'package:solidify/features/auth/sign_up/screens/engineer_account_sign_up/data/models/engineer_account_sign_up_request_model.dart';
+import 'package:solidify/features/auth/sign_up/screens/engineer_account_sign_up/data/models/engineer_account_sign_up_response_model.dart';
+import 'package:solidify/features/auth/sign_up/screens/concrete_company_account_sign_up/data/models/concrete_company_account_sign_up_response_model.dart';
+
+// Import the category response model
+
+
 part 'api_service.g.dart';
 
 @RestApi(baseUrl: ApiConstants.apiBaseUrl)
@@ -52,4 +57,10 @@ abstract class ApiService {
 
   @GET(ApiConstants.product)
   Future<ProductListResponseModel> productsList();
+
+  
+  @GET("Category/{id}")
+  Future<GetProductsByCategoryResponseModel> getCategory(
+    @Path("id") int id,
+  );
 }
