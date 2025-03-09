991f9756b3b14310a2ebdb4f5496b60e3d2a840f
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-03-09 06:27:20 +0200

    refactor the address screen

--- lib/features/marketplace/ui/screens/order/screens/payment_screen.dart
+++ lib/features/marketplace/ui/screens/order/screens/payment_screen.dart
@@ -1,10 +1,41 @@
+import 'package:flutter_svg/svg.dart';
 import 'package:flutter/material.dart';
+import 'package:solidify/core/helpers/extensions.dart';
+import 'package:solidify/core/routes/routes_name.dart';
+import 'package:solidify/core/theming/text_styles.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PaymentScreen extends StatelessWidget {
   const PaymentScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: SvgPicture.asset('assets/svgs/back_arrow.svg'),
+        ),
+        title: Text(
+          'Address details',
+          style: TextStyles.font18MainSemiBold,
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 8.w),
+            child: GestureDetector(
+              onTap: () => context.pushNamed(Routes.cartScreen),
+              child: SvgPicture.asset('assets/svgs/filled_cart.svg'),
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Text('paymentt'),
+        ),
+      ),
+    );
   }
 }
