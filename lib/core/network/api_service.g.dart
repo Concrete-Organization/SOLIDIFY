04b0a47e3f153eb48ea480f35009c535f5227aeb
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-03-10 06:58:22 +0200

    create the model and make the api call

--- lib/core/network/api_service.g.dart
+++ lib/core/network/api_service.g.dart
@@ -439,6 +439,39 @@ class _ApiService implements ApiService {
     return _value;
   }
 
+  @override
+  Future<GetProductByIdResponseBody> getProductById(String productId) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<GetProductByIdResponseBody>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'Product/${productId}',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late GetProductByIdResponseBody _value;
+    try {
+      _value = GetProductByIdResponseBody.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
---
+++ lib/features/marketplace/data/models/get_product_by_id_response_body.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_product_by_id_response_body.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+GetProductByIdResponseBody _$GetProductByIdResponseBodyFromJson(
+        Map<String, dynamic> json) =>
+    GetProductByIdResponseBody(
+      isSucceeded: json['isSucceeded'] as bool,
+      statusCode: (json['statusCode'] as num).toInt(),
+      message: json['message'] as String,
+      model: ProductModel.fromJson(json['model'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$GetProductByIdResponseBodyToJson(
+        GetProductByIdResponseBody instance) =>
+    <String, dynamic>{
+      'isSucceeded': instance.isSucceeded,
+      'statusCode': instance.statusCode,
+      'message': instance.message,
+      'model': instance.model,
+    };
+
+ProductModel _$ProductModelFromJson(Map<String, dynamic> json) => ProductModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      price: (json['price'] as num).toDouble(),
+      description: json['description'] as String,
+      measurement: (json['measurement'] as num).toInt(),
+      imageUri: json['imageUri'] as String,
+      rate: (json['rate'] as num).toInt(),
+      discount: (json['discount'] as num).toInt(),
+      brandName: json['brandName'] as String?,
+    );
+
+Map<String, dynamic> _$ProductModelToJson(ProductModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'price': instance.price,
+      'description': instance.description,
+      'measurement': instance.measurement,
+      'imageUri': instance.imageUri,
+      'rate': instance.rate,
+      'discount': instance.discount,
+      'brandName': instance.brandName,
+    };
