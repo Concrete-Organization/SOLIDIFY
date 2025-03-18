369f007904864792e77ec499c5648dbfec9c78e8
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-03-18 06:26:02 +0200

    start the screen

--- lib/features/profile_company/ui/profile_company_screen.dart
+++ lib/features/profile_company/ui/profile_company_screen.dart
@@ -1,13 +1,30 @@
+import 'package:flutter_svg/svg.dart';
 import 'package:flutter/material.dart';
+import 'package:solidify/core/helpers/spacing.dart';
+import 'package:solidify/core/theming/color_manger.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:solidify/core/widgets/horizontal_divider.dart';
+import 'package:solidify/features/profile_company/ui/widgets/profile_top_bar.dart';
 
 class ProfileCompanyScreen extends StatelessWidget {
   const ProfileCompanyScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('profile'),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ProfileTopBar(),
+              verticalSpace(10),
+              HorizontalDivider(
+                color: ColorsManager.secondaryGold,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
