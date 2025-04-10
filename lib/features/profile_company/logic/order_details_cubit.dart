b74486f7270385736a79111c2967ca039c29eefa
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-04-10 14:54:17 +0200

    make the cubit and inject it

--- lib/features/profile_company/logic/order_details_cubit.dart
+++ lib/features/profile_company/logic/order_details_cubit.dart
@@ -1,1 +1,42 @@
-class OrderDetailsCubit {}
+import 'order_details_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:solidify/core/network/api_error_model.dart';
+import 'package:solidify/core/helpers/shared_pref_helper.dart';
+import 'package:solidify/features/profile_company/data/repos/order_details_repo.dart';
+
+class OrderDetailsCubit extends Cubit<OrderDetailsState> {
+  final OrderDetailsRepo _orderDetailsRepo;
+
+  OrderDetailsCubit(this._orderDetailsRepo)
+      : super(const OrderDetailsState.initial());
+
+  Future<void> getOrderById(String orderId) async {
+    emit(const OrderDetailsState.loading());
+
+    final token =
+        await SharedPrefHelper.getSecuredString(SharedPrefKeys.accessToken);
+    if (token.isEmpty) {
+      emit(OrderDetailsState.error(
+        error: ApiErrorModel(message: 'No authentication token found'),
+      ));
+      return;
+    }
+
+    final response = await _orderDetailsRepo.getOrderById(orderId, token);
+
+    response.when(
+      success: (response) {
+        if (response.isSucceeded) {
+          emit(OrderDetailsState.success(response: response));
+        } else {
+          emit(OrderDetailsState.error(
+            error: ApiErrorModel(message: response.message),
+          ));
+        }
+      },
+      failure: (error) {
+        emit(OrderDetailsState.error(error: error));
+      },
+    );
+  }
+}
